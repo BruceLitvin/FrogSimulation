c7ab9f7b70c69359a18e9c9b1272df0071eb3e52
BruceLitvin <brucel15@nycstudents.net>
2025-11-06 12:08:34 -0500

    baseline

---
+++ FrogSimulation.java
@@ -0,0 +1,18 @@
+public class FrogSimulation {
+private int goalDistance;
+private int maxHops;
+public FrogSimulation(int dist, int numHops){
+    goalDistance=dist;
+    maxHops=numHops;
+}
+private int hopDistance(){
+return 0;
+}
+public boolean simulate(){
+    return true;
+}
+public double runSimulations(int num){
+return 0;
+}
+}
+
