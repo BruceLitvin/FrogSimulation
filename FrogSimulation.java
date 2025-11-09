a8cd894798f0db421ff14a9fa7d337ad3e840c6d
BruceLitvin <brucel15@nycstudents.net>
2025-11-09 17:04:37 -0500

    in progress

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -1,22 +1,52 @@
 public class FrogSimulation {
-private int goalDistance;
-private int maxHops;
-public FrogSimulation(int dist, int numHops){
-    goalDistance=dist;
-    maxHops=numHops;
-}
-private int hopDistance(){
-return 0;
-}
-public boolean simulate(){
+    private int goalDistance;
+    private int maxHops;
 
-    FrogSimulation sim= new FrogSimulation(24,5);
+    public FrogSimulation(int dist, int numHops) {
+        goalDistance = dist;
+        maxHops = numHops;
+    }
 
-    return true;
+    private int hopDistance() {
+        int e = (int) (Math.random() * -20) + 10;
+        System.out.println(e);
+        return e;
+    }
+
+         public boolean simulate () {
+        int sum=0;
+        System.out.println(simulate());
+        for(int i=1; i<= maxHops ;i++ ){
+            int f=0;
+            int g=0;
+            sum=hopDistance();
+
+        }
+        if (sum>=goalDistance) return true;
+        else return false;
+
+
+
+        }
+            // run hop distance maxHops times and then add numbers
+    //if the end position is greater than goalDistance return true
+    //If Frog reaches negative position return false
+    // if frog doesn't reach goalDistance return false
+        /*
+        public double runSimulations( int num){
+            return 0;
+        }
+
+                */
+    public static void main(String[]args){
+        FrogSimulation instance= new FrogSimulation(24,5);
+        int sim=instance.hopDistance();
+        boolean s= instance.simulate();
+
+    }
+
+
 
 }
-public double runSimulations(int num){
-return 0;
-}
-}
+
 
---
+++ randomness.java
@@ -0,0 +1,26 @@
+ /* public class randomness
+{
+    public static void main(String[] args){
+        double b= (Math.random()*10)+16;
+        //System.out.print(b);
+        int e  =(int) (Math.random()*15)+10;
+        for (int i=0; i <e; i++){
+
+
+        }
+        if (e==1)
+            System.out.println(" I love Mom "+ e + " Time");
+        else System.out.println(" I love Mom "+ e + " Times");
+        //2
+        int a= (int) b;
+        //System.out.println(a);
+
+        //3
+        double c= (Math.random()*-20)+10;
+        int d= (int) c;
+        //System.out.println(d);
+
+    }
+
+}
+*/
