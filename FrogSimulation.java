5861ea65e73e3cc87aeb773d4d3588e283410ce8
BruceLitvin <brucel15@nycstudents.net>
2025-11-10 12:16:55 -0500

    possibly done

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -10,41 +10,47 @@ public class FrogSimulation {
     }
 
     private int hopDistance() {
-        int e = (int) (Math.random() * 40) -20;  // range is -20 to 20
+        int e = (int) (Math.random() * 40) - 10;  // range is -20 to 20
         //System.out.println(e);
         return e;
     }
 
-         public boolean simulate () {
-        int sum=0;
+    public boolean simulate() {
+        int sum = 0;
 
-        for(int i=1; i<= maxHops ;i++ ){
-            int hopDistance= hopDistance();
+        for (int i = 1; i <= maxHops; i++) {
+            int hopDistance = hopDistance();
             sum += hopDistance;
         }
-             System.out.println(sum);
-        if (sum>=goalDistance) return true;
+        //System.out.println(sum);
+        if (sum >= goalDistance) return true;
         else return false;
 
 
-
-        }
-            // run hop distance maxHops times and then add numbers
+    }
+    // run hop distance maxHops times and then add numbers
     //if the end position is greater than goalDistance return true
     //If Frog reaches negative position return false
     // if frog doesn't reach goalDistance return false
-        /*
-        public double runSimulations( int num){
-            return 0;
-        }
 
-                */
-    public static void main(String[]args){
-        FrogSimulation instance= new FrogSimulation(24,5);
-        int sim=instance.hopDistance();
-        boolean s= instance.simulate();
-        System.out.println(s);
+    public double runSimulations(int num) {
+       double attempt=0;
+        if (simulate()==true)
+            attempt++;
+         else return attempt;
 
+         return 100*(attempt/num);
+
+    }
+
+
+    public static void main(String[] args) {
+        FrogSimulation instance = new FrogSimulation(24, 5);
+        int sim = instance.hopDistance();
+        boolean s = instance.simulate();
+        //System.out.println(s);
+        double times= instance.runSimulations(400);
+        System.out.println(times);
     }
 
 
--- randomness.java
+++ randomness.java
@@ -1,4 +1,4 @@
-TEST CODE
+//TEST CODE
 
 /* public class randomness
 {
