0123bf2677de9cdf7945cb117dea8b9ff7e5e113
BruceLitvin <brucel15@nycstudents.net>
2025-11-14 11:59:22 -0500

    possibly done

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -1,53 +1,49 @@
 
 
 public class FrogSimulation {
-    private int goalDistance;
+    private int goalDistance; //declare ints
     private int maxHops;
 
-    public FrogSimulation(int dist, int numHops) {
+    public FrogSimulation(int dist, int numHops) { // utilize a constructor
         goalDistance = dist;
         maxHops = numHops;
     }
 
     private int hopDistance() {
-        int e = (int) (Math.random() * 40) - 10;  // range is -20 to 20
+        int e = (int) (Math.random() * 40) - 10;  // range is -10 to 30
         //System.out.println(e);
         return e;
     }
 
     public boolean simulate() {
-        int sum = 0;
+        int sum = 0; //start with an index of 0
 
-        for (int i = 1; i <= maxHops; i++) {
+        for (int i = 1; i <= maxHops; i++) { // go until max hops are reached and add whatever math.rand prints
             int hopDistance = hopDistance();
-            sum += hopDistance;
+            sum += hopDistance; // add the values up
         }
         //System.out.println(sum);
-        if (sum >= goalDistance) return true;
-        else return false;
+        if (sum >= goalDistance) return true; //return true if total higher than a value goaldistance
+        else return false; //otherwise return false
 
 
     }
-    // run hop distance maxHops times and then add numbers
-    //if the end position is greater than goalDistance return true
-    //If Frog reaches negative position return false
-    // if frog doesn't reach goalDistance return false
 
     public double runSimulations(int num) {
-        int attempt=0;
-      for (int i=1; i==num;i++) {
-          if (simulate()==true)
+        int attempt=0; // set up a counter as an int
+      for (int i=1; i<=num;i++) { //add 1 until num is reached
+          if (simulate()==true)// if true add 1
               attempt++;
-          else return attempt;
+          else return attempt; //otherwise dont add
       }
-         return (attempt/num);
+         return (attempt/num);//return total
 
     }
 
 
     public static void main(String[] args) {
-        FrogSimulation instance = new FrogSimulation(24, 5);
-        int sim = instance.hopDistance();
+        FrogSimulation instance = new FrogSimulation(24, 5);//make new instance to be able to call a method
+        int sim = instance.hopDistance();// call methods
         boolean s = instance.simulate();
        // System.out.println(s);
         double times= instance.runSimulations(400);
